57a400a198ada1ef77bc01cde8b0e0043abfe71f
Anton Pylypenko <anton_pylypenko@epam.com>
2025-02-27 09:36:59 +0200

    END OF SECTION 3: added sorting

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -1,3 +1,4 @@
+using System.Xml.Linq;
 using Core.Entities;
 using Core.Interfaces;
 using Infrastructure.Data;
@@ -25,7 +26,7 @@ public class ProductRepository(StoreContext context) : IProductRepository
         return await context.Products.FindAsync(id);
     }
 
-    public async Task<IReadOnlyList<Product>> GetProductsAsync(string? brand, string? type)
+    public async Task<IReadOnlyList<Product>> GetProductsAsync(string? brand, string? type, string? sort)
     {
         var query = context.Products.AsQueryable();
 
@@ -34,6 +35,14 @@ public class ProductRepository(StoreContext context) : IProductRepository
 
         if (!string.IsNullOrWhiteSpace(type))
             query = query.Where(x => x.Type == type);
+
+    
+        query = sort switch
+        {
+            "priceAsc" => query.OrderBy(x => x.Price),
+            "priceDesc" => query.OrderByDescending(x => x.Price),
+            _ => query.OrderBy(x => x.Name)
+        }; 
 
         return await query.ToListAsync();
     }
