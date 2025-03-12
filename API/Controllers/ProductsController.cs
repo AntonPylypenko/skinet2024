3b4c3de38bc74af2e698db05311026cde84a869c
Anton Pylypenko <anton_pylypenko@epam.com>
2025-03-12 16:20:50 +0200

    END OF SECTION 4: Added to Specification possibility of rpoduct projection (Select etc.)

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -81,15 +81,17 @@ public class ProductsController(IGenericRepository<Product> repo) : ControllerBa
     [HttpGet("brands")]
     public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
     {
-        //TODO
-        return Ok();
+        var spec = new BrandListSpecification();
+
+        return Ok(await repo.ListAsync(spec));
     }
 
     [HttpGet("types")]
     public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
     {
-        //TODO
-        return Ok();
+        var spec = new TypeListSpecification();
+
+        return Ok(await repo.ListAsync(spec));
     }
 
     private bool ProductExist(int id) 
--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -24,6 +24,45 @@ public class SpecificationEvaluator<T> where T : BaseEntity
             query = query.OrderByDescending(spec.OrderByDescending);
         }
 
+        if (spec.IsDistinct)
+        {
+            query = query.Distinct();
+        }
+
         return query;
+    } 
+
+    public static IQueryable<TResult> GetQuery<TSpec, TResult>(IQueryable<T> query, ISpecification<T, TResult> spec)
+    {
+        // обов'язково роби перевірку на нул, бо буде помилка 500 при запиті
+        // (Internal Server Error. System.ArgumentNullException: Value cannot be null. )
+        if (spec.Criteria != null)
+        {
+            query = query.Where(spec.Criteria);
+        }
+
+        if (spec.OrderBy != null)
+        {
+            query = query.OrderBy(spec.OrderBy);
+        }
+
+        if (spec.OrderByDescending != null)
+        {
+            query = query.OrderByDescending(spec.OrderByDescending);
+        }
+
+        var selectQuery = query as IQueryable<TResult>;
+
+        if (spec.Select != null)
+        {
+            selectQuery = query.Select(spec.Select);
+        }
+
+        if (spec.IsDistinct)
+        {
+            selectQuery = selectQuery?.Distinct();
+        }
+
+        return selectQuery ?? query.Cast<TResult>();
     }
 }
