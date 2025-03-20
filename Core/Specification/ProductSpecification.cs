273747063006e9989c3bb1054293beff2b61c835
Anton Pylypenko <anton_pylypenko@epam.com>
2025-03-20 08:41:37 +0200

    END OF SECTION 5: added search

--- Core/Specification/ProductSpecification.cs
+++ Core/Specification/ProductSpecification.cs
@@ -5,6 +5,7 @@ namespace Core.Specification;
 public class ProductSpecification : BaseSpecification<Product>
 {
     public ProductSpecification(ProductSpecParams productSpecParams) : base(x => 
+        (string.IsNullOrEmpty(productSpecParams.Search) || x.Name.ToLower().Contains(productSpecParams.Search)) &&
         (productSpecParams.Brands.Count == 0  || productSpecParams.Brands.Contains(x.Brand)) &&
         (productSpecParams.Types.Count == 0  || productSpecParams.Types.Contains(x.Type)) 
     )
