72405eb0c379b2a01325d20bc54dd8497c1fea4e
Anton Pylypenko <anton_pylypenko@epam.com>
2025-04-07 08:06:44 +0300

    SECTION 6: added ERROR handling

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -16,13 +17,13 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline. (MIDDLEWARE)
-
+app.UseMiddleware<ExceptionMiddleware>();
 app.MapControllers();
 
 try
 {
    using var scope = app.Services.CreateScope();
-   var services= scope.ServiceProvider;
+   var services = scope.ServiceProvider;
    var context = services.GetRequiredService<StoreContext>();
    await context.Database.MigrateAsync();
    await StoreContextSeed.SeedAsync(context); 
