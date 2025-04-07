060afbef2cae280a32042f54cd60a7e65ba4c254
Anton Pylypenko <anton_pylypenko@epam.com>
2025-04-07 10:06:59 +0300

    END OF SECTION 6: added CORS support

--- API/Program.cs
+++ API/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<StoreContext>(opt => {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddCors();
 
 // builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));  
 
@@ -18,6 +19,9 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline. (MIDDLEWARE)
 app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 app.MapControllers();
 
 try
